3b049a7674106012739f56f59c983ca0b135426d
Malmevik <jonas.malmevik@gmail.com>
2022-08-14 22:51:39 +0200

    Completed challenges 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (input, repeatTimes) {
+  if (repeatTimes < 0) return "ERROR";
 
+  let text = "";
+  for (let index = 0; index < repeatTimes; index++) {
+    text += input;
+  }
+  return text;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  let reversedText = "";
+  for (let i = 0; i < text.length; i++) {
+    reversedText += text[text.length - 1 - i];
+  }
+  return reversedText;
 };
 
 // Do not edit below this line
